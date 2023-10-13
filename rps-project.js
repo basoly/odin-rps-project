c1374ab89eb67a212a72348cf648212c91657229
LBD <lyanbasora@gmail.com>
2023-10-13 11:09:52 -0400

    Add comments detailing code logic before writing code

---
+++ rps-project.js
@@ -0,0 +1,23 @@
+function getComputerChoice() {
+    let options = ['Rock', 'Paper', 'Scissors'];
+    // use Math.random.floor * 3 to choose number from 0 to choose random inde
+
+}
+
+function playGame(playerSelection, computerSection) {
+    //function for single round of RPS
+    playerChoice = getPlayerChoice();
+    computerChoice = getComputerChoice();
+    // conditionals: choice evaluation
+    // if player has winning choice (playerchoice == winning choice)
+    // print You win. {choice} beats {choice}.
+
+}
+
+function game() {
+    // Loop 5 rounds
+    // use prompt() to prompt player for choice
+    // sanitize user input by making all lowercase
+    // previous functions go here
+    // use console log to print the results of each round.
+}
