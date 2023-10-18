a4ee07b1680f341ffa8af55be50008d02abf4178
LBD <lyanbasora@gmail.com>
2023-10-18 00:37:03 -0400

    Add new playGame() function logic, Clean up code comments

--- rps-project.js
+++ rps-project.js
@@ -1,70 +1,85 @@
 function random(number) {
-    //number between 0 and specified index number
     return Math.floor(Math.random() * number);
 }
 
 function getComputerChoice() {
-    let options = ['Rock', 'Paper', 'Scissors'];
-    let computerChoice = options[random(2)];
-    // use Math.random.floor * 2 to choose number from 0 to choose index of array
-    return computerChoice
+    let options = ['rock', 'paper', 'scissors'];
+    let computerSelection = options[random(2)];
+    return computerSelection
 }
 
 function playGame(playerSelection) {
-    //function for single round of RPS
-
-    // playerChoice = getPlayerChoice();
-
-    computerSelection = getComputerChoice(); // get computer choice
-
-    // conditionals: choice evaluation
-    //switch statement with different cases
-
-    switch(playerSelection) {
-        case 'rock':
-            if (computerSelection == 'scissors') {
-                console.log(`You win. ${playerSelection} beats ${computerSelection}!`);
-            break;
-            }
-            
-        case 'paper':
-            if (computerSelection == 'rock') {
-                console.log(`You win. ${playerSelection} beats ${computerSelection}!`);
-                break;
-            }
-            
-        case 'scissors':
-            if (computerSelection == 'paper') {
-                console.log(`You win. ${playerSelection} beats ${computerSelection}`);
-                break;
-            }
-            
-        case computerSelection:
-            console.log(`It's a tie. The player chose ${playerSelection} and the computer chose ${computerSelection}`);
-            break;
-        
-        default:
-            console.log(`You lose. ${computerSelection} beats ${playerSelection} (your option)`)
-            break;
-
+    const validOptions = ["rock", "paper", "scissors"]
+    if (!(validOptions.includes(playerSelection.toLowerCase))) {
+        "Invalid input. Please try again"
+        let result = "invalid input"
+        return result
     }
-    
-    // if player has winning choice (playerchoice == winning choice)
-    // print You win. {choice} beats {choice}.
+
+    let winner;
+    let computerSelection;
+    computerSelection = getComputerChoice().toLowerCase();
+    console.log(`The Computer chose ${computerSelection}`)
+    playerSelection = playerSelection.toLowerCase();
+    console.log(`The player chose ${playerSelection}`)
+
+    if ((playerSelection == 'rock' && computerSelection == 'scissors') 
+    || (playerSelection == 'paper' && computerSelection == 'rock') 
+    || (playerSelection == 'scissors' && computerSelection == 'paper')) {
+        // win message 
+        console.log(`You win. ${playerSelection} beats ${computerSelection}!`);
+        winner = 'player'
+        return winner
+        
+    } else if (playerSelection == computerSelection) {
+        // tie message
+        console.log(`It's a tie. The player chose ${playerSelection} and the computer chose ${computerSelection}`);
+        winner = 'tie'
+        return winner
+    } else {
+        // lose message
+        console.log(`You lose. ${computerSelection} beats ${playerSelection} (your option)`);
+        winner = 'computer'
+        return winner
+    }
+
 
 }
 
 function game() {
     // Loop 5 rounds
     alert('Start of Game')
+    let playerScore = 0;
+    let computerScore = 0;
+    let result;
+    let playerSelection;
+    
+
     for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt();
-        playGame(); 
+        playerSelection = prompt();
+        result = playGame(playerSelection);
+        if (result == 'player') {
+            playerScore++
+        } else if (result == 'computer') {
+            computerScore++
+        } else if (result == 'invalid input') {
+            console.log("invalid input. Please try again.")
+            i--
+            continue   
+        } else {
+            continue
+        }
+        
     }
 
+    console.log(`Player Score: ${playerScore}`);
+    console.log(`Computer Score: ${computerScore}`);
+    if (playerScore > computerScore) {
+        console.log(`Player wins. Player score: ${playerScore}, Computer score: ${computerScore}`)
+    } else if (playerScore == computerScore) {
+        console.log(`It's a tie. Player score: ${playerScore}, Computer score: ${computerScore}`)
+    } else {
+        console.log(`Computer wins. Player score: ${playerScore}, Computer score: ${computerScore}`)
+    }
 
-    // use prompt() to prompt player for choice
-    // sanitize user input by making all lowercase
-    // previous functions go here
-    // use console log to print the results of each round.
 }
